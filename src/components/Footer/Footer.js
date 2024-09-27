c790f63d8e62c686e29bc22d2dd645137f41e203
Valdemar <nikiplaycar@gmail.com>
2024-09-27 16:43:16 +0300

    feat: add basic Header and Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,120 @@
 import React, { Component } from 'react';
-import './Footer.css';
+import './Footer.css'; // Импортируйте CSS-файл
 
 class Footer extends Component {
   render() {
     return (
       <footer>
-        <p>© 2024 Ваше имя. Все права защищены.</p>
+        <div className="FooterRectangle"></div>
+        <div className="FooterRecG"></div>
+        <div className="FooterRecV"></div>
+        <div className="FooterDownRight"></div>
+        <div className="FooterTop2">
+          <div className="FooterTop2text" data-i18n="we_treat_your_eyes_with_care">
+            We treat your eyes with care
+          </div>
+        </div>
+        <div className="FooterTop1">
+          <a href="../HomePage/index.html" className="FooterTop1text">yes.</a>
+        </div>
+        <div className="Footerbase1">
+          <div className="Footerbase1text">Designed and built by Margarita</div>
+        </div>
+        <div className="Footerbase2">
+          <div className="Footerbase2text">2020 Yes for Eyes</div>
+        </div>
+        <div className="Footerbase3">
+          <img src="Images/Vector50.svg" className="Footerbase3v" alt="Vector Image" />
+        </div>
+        <div className="Footerbase4">
+          <a href="#Shop" className="Footerbase4text">Shop</a>
+        </div>
+        <div className="Footerbase5">
+          <a href="/Custom/Custom.html" className="Footerbase5text" data-i18n="custom_care">Customer care</a>
+        </div>
+        <div className="Footerbase6">
+          <a href="#Info" className="Footerbase6text">Info</a>
+        </div>
+        <div className="Footerbase7">
+          <a href="/Sun/Sun.html" className="Footerbase7text" data-i18n="sun">Sun</a>
+        </div>
+        <div className="Footerbase8">
+          <a href="/Optical/Optical.html" className="Footerbase8text" data-i18n="optical">Optical</a>
+        </div>
+        <div className="Footerbase9">
+          <a href="#Brands" className="Footerbase9text">Brands</a>
+        </div>
+        <div className="Footerbase10">
+          <a href="../Privacy/privacy.html" className="Footerbase10text">FAQ</a>
+        </div>
+        <div className="Footerbase11">
+          <div className="Footerbase11text">Shipping and returns</div>
+        </div>
+        <div className="Footerbase12">
+          <div className="Footerbase12text" data-i18n="fit_guide">Fit guide</div>
+        </div>
+        <div className="Footerbase13">
+          <a href="../Privacy/privacy.html" className="Footerbase13text" data-i18n="Terms_and_conditions">Terms and conditions</a>
+        </div>
+        <div className="Footerbase14">
+          <a href="../Privacy/privacy.html" className="Footerbase14text" data-i18n="Privacy_Policy">Privacy policy</a>
+        </div>
+        <div className="Footerbase15">
+          <div className="Footerbase15text">We are always close</div>
+        </div>
+        <div className="Footerbase16">
+          <div className="Footerbase16text">No spam, just pure inspiration and good news</div>
+        </div>
+        <div className="Footerbase17">
+          <div className="Footerbase17text">This site is a design concept and not meant for commercial purposes. I don't own or sell any goods displayed on this site.</div>
+        </div>
+        <div className="Footerbase18" onClick={() => this.callPhoneNumber('+123456789')}>
+          <div className="Footerbase18text">+375 44 538 6581</div>
+        </div>
+        <div className="Footerbase19">
+          <a href="mailto:your-email@example.com?subject=Subject&body=Message%20Body" className="Footerbase19text">nikiplaycar@gmail.com</a>
+        </div>
+        <div className="FooterDown">
+          <div className="FooterDownRec1"></div>
+          <div className="FooterDownRec2"></div>
+          <div className="FooterDownRec3"></div>
+          <div className="FooterDownRec4"></div>
+          <div className="FooterDownRec5"></div>
+          <div className="Footercomma1">,</div>
+          <div className="Footercomma2">,</div>
+          <div className="Footercomma3">,</div>
+          <div className="Footercomma4">,</div>
+          <div className="Footerdowntext">Photo materials belong to</div>
+          <div className="Footerdown1">
+            <div className="Footerdown1text">Unsplash</div>
+          </div>
+          <div className="Footerdown2">
+            <div className="Footerdown2text">Modern Legacy</div>
+          </div>
+          <div className="Footerdown3">
+            <div className="Footerdown3text">TIJN</div>
+          </div>
+          <div className="Footerdown4">
+            <div className="Footerdown4text">Carla Colour</div>
+          </div>
+          <div className="Footerdown5">
+            <div className="Footerdown5text">Krewe</div>
+          </div>
+        </div>
+        <div className="FooterLogoInst">
+          <img src="Images/Vector51.svg" className="FooterLogoInstv1" alt="Vector Image" />
+          <img src="Images/Vector52.svg" className="FooterLogoInstv2" alt="Vector Image" />
+        </div>
+        <a href="https://github.com/Penguin-Worker" className="FooterLogoFB">
+          <img src="Images/Vector53.svg" className="FooterLogoFBv" alt="Vector Image" />
+        </a>
       </footer>
     );
+  }
+
+  callPhoneNumber(number) {
+    // Здесь можно добавить функциональность для обработки клика на номер телефона
+    alert(`Calling ${number}`);
   }
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,77 @@
 import React, { Component } from 'react';
-import './Header.css';
+import './Header.css'; // Импортируйте CSS-файл
 
 class Header extends Component {
   render() {
     return (
-      <header className="App-header">
-        <h1>Заголовок приложения</h1>
-        <h2>Название организации: {this.props.companyName}</h2>
+      <header>
+        <div className="MenuHigh">
+          <nav>
+            <div className="mobile-only-content" id="burger-menu1">
+              <div className="TABmenu">
+                <div className="burger-menu-content" id="burger-menu-content">
+                  <nav className="BG">
+                    <ul className="burger-menu-links">
+                      <li><a href="../HomePage/index.html" data-i18n="home">Home</a></li>
+                      <li><a href="../Sun/Sun.html" data-i18n="sun">Sun</a></li>
+                      <li><a href="../Optical/Optical.html" data-i18n="optical">Optical</a></li>
+                      <li><a href="../Custom/Custom.html" data-i18n="fit_guide">Fit guide</a></li>
+                      <li><a href="../Privacy/privacy.html" data-i18n="Privac_Policy">Privacy Policy</a></li>
+                      <li><a href="../Privacy/privacy.html" data-i18n="Terms_of_Service">Terms of Service</a></li>
+                      <li><button id="theme-toggle" className="StyleBT">T</button></li>
+                      <li><button id="openRegisterModal" className="StyleB"><span className="i18n signup">Sign up</span></button></li>
+                      <li><button id="openLoginModal" className="StyleB"><span className="i18n login">Log in</span></button></li>
+                      <li><button id="logoutButton" className="StyleB"><span className="i18n logout">Log out</span></button></li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="rectangleM1"></div>
+                <div className="rectangleM2"></div>
+                <div className="rectangleM3"></div>
+              </div>
+              <div className="rectangleM4"></div>
+              <div className="rectangleM5"></div>
+            </div>
+            <div className="Menu1">
+              <div className="rectangle-9"></div>
+              <a href="../Custom/Custom.html" className="Menu1text" data-i18n="customer_care">Customer care</a>
+            </div>
+            <div className="Menu2">
+              <div className="rectangle-4"></div>
+              <a href="../Privacy/privacy.html" className="Menu2text" data-i18n="account">Account</a>
+            </div>
+            <div className="Menu3">
+              <div className="rectangle-5"></div>
+              <a href="../Optical/Optical.html" className="Menu3text" data-i18n="optical">Optical</a>
+            </div>
+            <div className="Menu4">
+              <div className="rectangle-6"></div>
+              <a href="../HomePage/index.html" className="Menu4text">yes.</a>
+            </div>
+            <div className="Menu5">
+              <div className="rectangle-7"></div>
+              <a href="../Sun/Sun.html" className="Menu5text" data-i18n="sun">Sun</a>
+            </div>
+            <div className="Menu6">
+              <div className="rectangle-8"></div>
+              <a href="../Custom/Custom.html" className="Menu6text" data-i18n="fit_guide">Fit guide</a>
+            </div>
+          </nav>
+          <div className="animation-container">
+            <div className="cube">
+              <div className="side front"></div>
+              <div className="side back"></div>
+              <div className="side left"></div>
+              <div className="side right"></div>
+              <div className="side top"></div>
+              <div className="side bottom"></div>
+            </div>
+            {/* Повторите для других кубов, если нужно */}
+          </div>
+          <div className="rectangle-1"></div>
+          <div className="rectangle-2"></div>
+        </div>
+        
       </header>
     );
   }
